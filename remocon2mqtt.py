eed73be9e080688d077bf7c107f5962afd53f314
nechry <nechry@gmail.com>
2022-09-12 16:08:19 +0200

    initial commit

--- remocon2mqtt.py
+++ remocon2mqtt.py
@@ -1,42 +1,37 @@
+import sys
 import requests
 import json
 import paho.mqtt.client as mqtt
 from urllib.parse import quote
 import configparser
 
-
 def on_connect(client, userdata, flags, rc):
-    print("Connected with result code "+str(rc))
-    pass
+    print(f'Connected {str(rc)}')
 
 def on_publish(client, userdata, result):
-    print("Data published")
-    pass
+    print("Data published with success")
 
 def on_disconnect(client, userdata, rc):
-    print("disconnecting reason  " + str(rc))
-    pass
+    print(f'Disconnect, reason {str(rc)}')
 
 def connectMQTT(config):
     client = mqtt.Client()
     client.on_connect = on_connect
     client.on_publish = on_publish
     client.on_disconnect = on_disconnect
+
     client.username_pw_set(username=config.get('MQTT','username'), password=config.get('MQTT','password'))
+
     try:
         client.connect(config.get('MQTT','host'), config.getint('MQTT','port'), 60)
-    except Exception as e: 
-        print(e)
-        print("ERROR: Can not connect to MQTT broker")
-        return -1
+    except Exception as error: 
+        print(f'ERROR: Can not connect to MQTT broker {error}')
+        sys.exit(1)
     return client
 
 
 def sendHVACtoMQTT(config, data):
     client = connectMQTT(config)
-    if client == -1:
-        return -1
-
     client.publish(config.get('MQTT','topic'), json.dumps(data))  # publish
     client.disconnect()
 
@@ -50,25 +45,31 @@ def main(config):
     session = requests.session()
     # login,get session cookie and address for json data
     response = session.post(url=url, headers=headers, data=payload)
-    gateway = config.get('REMOCON-NET','gateway')
-    
-    url = f"{base_url}R2/PlantHomeBsb/GetData/{gateway}"
-    # get zone 1 data 
-    payload = {
-        "useCache": True, 
-        "zone": 1, 
-        "filter": 
-            {
-                "progIds": "null", 
-                "plant": True, 
-                "zone": True
-             }
-        }
-
-    response = session.post(url=url, json=payload)
-    result_json = json.loads(response.text)        
-    sendHVACtoMQTT(config=config, data=result_json['data'])
-
+    result_json = json.loads(response.text)    
+    if(result_json['ok']):
+        gateway = config.get('REMOCON-NET','gateway')        
+        url = f"{base_url}R2/PlantHomeBsb/GetData/{gateway}"
+        # get zone 1 data 
+        payload = {
+            "useCache": True, 
+            "zone": 1, 
+            "filter": 
+                {
+                    "progIds": "null", 
+                    "plant": True, 
+                    "zone": True
+                }
+            }
+        response = session.post(url=url, json=payload)
+        if(response.status_code==200):    
+            result_json = json.loads(response.text)
+            sendHVACtoMQTT(config=config, data=result_json['data'])
+            return
+        else:
+            print(response.text)
+    else:
+        print(result_json['message'])
+    sys.exit(1)
 
 if __name__ == '__main__':        
     config = configparser.ConfigParser()
